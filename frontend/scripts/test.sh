9320bf4d5342da14ec0d942f6745d2d3f9e77fb5
Leykh <eykhell@gmail.com>
2024-08-28 23:19:53 +0200

    added in front scripts

---
+++ frontend/scripts/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# npm install --save-dev cross-env
+
+#set -x
+echo 'runner Jest (https://project-collab.github.io/jest/).'
+echo 'ROUTES OK'
+echo 'SERVEUR OK'
+echo 'APP OK'
+
+#npm test
