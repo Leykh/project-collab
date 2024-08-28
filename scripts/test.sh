e9d358653287a41798dbf81aa96445295d9c2ecd
Leykh <eykhell@gmail.com>
2024-08-28 22:36:11 +0200

    permission added

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,9 +2,10 @@
 
 # npm install --save-dev cross-env
 
+set -x
 echo 'runner Jest (https://project-collab.github.io/jest/).'
 echo 'ROUTES OK'
 echo 'SERVEUR OK'
 echo 'APP OK'
-#set -x
+
 #npm test
